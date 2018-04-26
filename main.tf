b60a4c302a2f307feaf0c3b68cb284bf00a28ac0
Clay Shekleton <clayshek@gmail.com>
2018-04-25 21:54:08 -0400

    Add GPU Memory Optimization

--- main.tf
+++ main.tf
@@ -39,6 +39,9 @@ resource "null_resource" "raspberry_pi_bootstrap" {
       # COPY KUBERNETES PREP SCRIPT
       "curl https://raw.githubusercontent.com/clayshek/terraform-raspberrypi-bootstrap/master/k8s_prep.sh > /home/pi/k8s_prep.sh",
 
+      # OPTIMIZE GPU MEMORY
+      "echo "gpu_mem=16" | sudo tee -a /boot/config.txt",
+
       # REBOOT
       "sudo reboot"
     ]
