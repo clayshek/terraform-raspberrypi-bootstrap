123fe5790428512a43aa1b46de553c6bbe456db2
Clay Shekleton <clayshek@users.noreply.github.com>
2019-02-13 21:34:53 -0500

    Add Helm & Tiller Init Scripts

--- main.tf
+++ main.tf
@@ -41,6 +41,11 @@ resource "null_resource" "raspberry_pi_bootstrap" {
       # COPY KUBERNETES PREP SCRIPT
       "curl https://raw.githubusercontent.com/clayshek/terraform-raspberrypi-bootstrap/master/k8s_prep.sh > /home/pi/k8s_prep.sh",
       "chmod u+x k8s_prep.sh",
+      
+      # COPY HELM & TILLER SETUP
+      "curl https://raw.githubusercontent.com/clayshek/terraform-raspberrypi-bootstrap/master/helm/tiller-rbac-config.yml > /home/pi/helm/tiller-rbac-config.yml",
+      "curl https://raw.githubusercontent.com/clayshek/terraform-raspberrypi-bootstrap/master/helm/install-helm-tiller.sh > /home/pi/helm/install-helm-tiller.sh",
+      "chmod u+x helm/install-helm-tiller.sh",
 
       # OPTIMIZE GPU MEMORY
       "echo 'gpu_mem=16' | sudo tee -a /boot/config.txt",
